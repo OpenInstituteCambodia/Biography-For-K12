752f8b5d3901a9476144f00d04f54d9923838fa7
Socheat Sok <socheatsok78@gmail.com>
2017-03-13 20:49:30 +0700

    update db.ts, package.json

--- app/src/components/db.ts
+++ app/src/components/db.ts
@@ -6,7 +6,23 @@ declare var cordova: any;
 
 @Injectable()
 export class DatabaseController {
+  private _db = cordova.file.applicationDirectory+'www/assets/files/database/appdata.db';
 
-  constructor() {}
+  private _db_path = '/www/assets/files/database/appdata.db';
+  // private _db_path = File.listDir(cordova.file.applicationDirectory, 'www/assets/files/database').then( suc => console.log(suc) ).catch(err => console.log(err));
+
+  constructor() {
+    console.log(this._db);
+    console.log(this._db_path);
+
+    this.initDB().then( suc => console.log(suc) ).catch( err => console.log(err) );
+  }
+
+  public initDB() {
+    return SQLite.openDatabase({
+      name: this._db_path,
+      location: 'default'
+    });
+  }
 
 }
